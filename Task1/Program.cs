5ca3368b5b88de55c7f7f1f8de505f9e5f347c46
Natalya <andreyevani@yandex.ru>
2022-12-03 16:06:56 +0300

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []
+*/
+
+string[] GetFinalArray(string[] input, int n)
+{
+    string[] output = new string[CountSymbolsOfElement(input, n)];
+    for (int i = 0, j = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+    return output;
+}
+
+int CountSymbolsOfElement(string[] input, int k)
+{
+    int count = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= k)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] InputArray()
+{
+    Console.Write("Введите значения для массива через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
+
+void Execute()
+{
+    string[] inputArray = InputArray();
+    string[] resultArray = GetFinalArray(inputArray, 3);
+    Console.WriteLine($" Исходный массив: [{string.Join("; ", inputArray)}]  Итоговый массив: [{string.Join("; ", resultArray)}]");
+}
+Execute();
